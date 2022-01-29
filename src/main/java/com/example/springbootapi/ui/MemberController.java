6fe2cf06a2a5054e83e4f96fc166b4df190d8c68
JuHyun419 <zzang9ha@naver.com>
2022-01-29 18:03:32 +0900

    add api samples

--- src/main/java/com/example/springbootapi/ui/MemberController.java
+++ src/main/java/com/example/springbootapi/ui/MemberController.java
@@ -36,11 +36,19 @@ public class MemberController {
 
 
     /**
-     * 회원 삭제 API
+     * 특정 회원 삭제 API
      * [DELETE] /api/members/{memberId}
      * @param memberId
      * @return
      */
 
+
+
+    /**
+     * 전체 회원 삭제 API
+     * [DELETE] /api/members
+     * @param memberId
+     * @return
+     */
 
 }
